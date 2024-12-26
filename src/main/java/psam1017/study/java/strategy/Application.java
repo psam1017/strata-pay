569eeb5c2e78f7b2cbb81cda3765ed0df3074eb9
sam_nodeblack <psam1017@nodeblack.io>
2024-12-26 13:23:54 +0900

    Chore: Initial Commit

---
+++ src/main/java/psam1017/study/java/strategy/Application.java
@@ -0,0 +1,13 @@
+package psam1017.study.java.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/psam1017/study/java/strategy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package psam1017.study.java.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
